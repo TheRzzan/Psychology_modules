35c4f2c08b8877e907c8ab60bcfb534f26122cae
Yevgeniy Morozov <morozov.e@edu.spbstu.ru>
2019-09-25 09:20:01 +0300

    Bottom bar added.

--- app/src/main/java/com/morozov/psychology/MainActivity.kt
+++ app/src/main/java/com/morozov/psychology/MainActivity.kt
@@ -9,16 +9,24 @@ class MainActivity : AppCompatActivity() {
 
     private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
         when (item.itemId) {
-            R.id.navigation_home -> {
-                message.setText(R.string.title_home)
+            R.id.navigation_experiments -> {
+                //
                 return@OnNavigationItemSelectedListener true
             }
-            R.id.navigation_dashboard -> {
-                message.setText(R.string.title_dashboard)
+            R.id.navigation_tests -> {
+                //
                 return@OnNavigationItemSelectedListener true
             }
-            R.id.navigation_notifications -> {
-                message.setText(R.string.title_notifications)
+            R.id.navigation_diary -> {
+                //
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.navigation_change -> {
+                //
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.navigation_profile -> {
+                //
                 return@OnNavigationItemSelectedListener true
             }
         }
